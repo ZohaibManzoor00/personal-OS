6a733b7fbff8fdd8bb30225874facaad540a9af6
zohaibmanzoor00 <zohaibmanzoor1700@gmail.com>
2026-05-08 13:30:20 -0400

    update: bg jobs with inngest setupg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
-'use client';
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export default function Home() {
-  const signOut = async () => {
-    await authClient.signOut();
-  };
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => toast.success("Workflow created"),
+    }),
+  );
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button onClick={signOut}>Sign Out</Button>
+      {data?.map((workflow) => <div key={workflow.id}>{workflow.name}</div>)}
+      <Button onClick={() => create.mutate({ name: "test" })}>Create Workflow</Button>
     </div>
   );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,24 @@
+import { inngest } from "@/inngest/client";
 import { prisma } from "@/lib/db";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import z from "zod";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(async ({ ctx }) => {
-    console.log({ userId: ctx.session.user.id, email: ctx.session.user.email });
-    const users = await prisma.user.findMany();
-    return users;
+  getWorkflows: protectedProcedure.query(async () => {
+    const workflows = await prisma.workflow.findMany();
+    return workflows;
+  }),
+  createWorkflow: protectedProcedure.input(z.object({ name: z.string() })).mutation(async () => {
+    await inngest.send({
+      name: "app/task.created",
+      data: { id: "123", name: "John Doe" },
+    });
+    console.log("workflow created");
+
+    await prisma.workflow.create({ data: { name: "test" } });
+    console.log("workflow created in database");
+
+    return { message: "Workflow created" };
   }),
 });
 
